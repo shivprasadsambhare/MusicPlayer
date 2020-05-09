da4584c6e96f6c6452a253c5034fa163b9351bb0
Shivani <pshivani307@gmail.com>
2020-05-09 16:06:46 +0530

    added logo and background

--- src/components/components/Header/index.js
+++ src/components/components/Header/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import Logo from '../../../snaps/musicLogo.jpg';
+import '../../../styles/styles.scss';
 
 const { Header } = Layout;
 
 function customHeader() {
   return (
-    <Header>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Home</Menu.Item>
-        <Menu.Item key="2">Search</Menu.Item>
-        <Menu.Item key="3">Profile</Menu.Item>
-      </Menu>
-    </Header>
+    <div>
+      <img src={Logo} alt="" className="Logo" />
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Search</Menu.Item>
+          <Menu.Item key="3">Profile</Menu.Item>
+        </Menu>
+      </Header>
+    </div>
   );
 }
 
